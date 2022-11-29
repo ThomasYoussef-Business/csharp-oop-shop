58fbb2cdeb30bc36f73dd3cbc6d568fa7584c1cf
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-11-29 15:26:27 +0100

    Creato scheletro per il mio programma usando auto-accessori e metodi che danno errore "NotImplementedException" se chiamati

---
+++ Tipi/Prodotto.cs
@@ -0,0 +1,20 @@
+﻿namespace Tipi {
+    public class Prodotto {
+        public string Codice { get; init; }
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public int PrezzoBase { get; init; }
+        public int Iva { get; init; }
+        public int PrezzoConIva {
+            get {
+                throw new NotImplementedException();
+            }
+        }
+
+
+        public Prodotto() {
+            throw new NotImplementedException();
+        }
+
+    }
+}
