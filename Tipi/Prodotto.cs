6afcbb3ff20cbc781024b1bdf54d0658aeaf6491
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-11-29 15:40:42 +0100

    Implementata Iva con controllo

--- Tipi/Prodotto.cs
+++ Tipi/Prodotto.cs
@@ -3,13 +3,17 @@
         public string Codice { get; init; }
         public string Nome { get; set; }
         public string Descrizione { get; set; }
-        public int PrezzoBase { get; init; }
-        public int Iva { get; init; }
-        public int PrezzoConIva {
-            get {
-                throw new NotImplementedException();
+        public float PrezzoBase { get; init; }
+        private readonly float iva;
+        public float Iva {
+            get => iva;
+            init {
+                if (value is < 0 or > 1) {
+                    throw new ArgumentOutOfRangeException(nameof(value), $"{nameof(value)} deve essere tra 0 e 1");
+                }
             }
         }
+        public float PrezzoConIva { get => (PrezzoBase * Iva); }
 
 
         public Prodotto() {
